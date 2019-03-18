fa3811fdea368afeba4aa763b3abdafbb0de382d
jinxing <1622152930@qq.com>
2019-03-18 20:34:07 +0800

    配置eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,43 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: 'airbnb-base',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['vue'],
-  rules: {},
+    env: {
+        browser: true,
+        es6: true
+    },
+    extends: "airbnb-base",
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+        parser: "babel-eslint"
+    },
+    plugins: ["vue"],
+    rules: {
+        // allow paren-less arrow functions
+        "arrow-parens": 0,
+        // allow async-await
+        "generator-star-spacing": "off",
+        // allow debugger during development
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "eol-last": 0,
+        "space-before-function-paren": 0,
+        // 配置四格缩进
+        "indent": "off",
+        "vue/script-indent": [
+            "error",
+            4,
+            {
+                "baseIndent": 1
+            }
+        ],
+        "vue/html-indent": [
+            "error",
+            4,
+            {
+                "baseIndent": 1
+            }
+        ]
+    }
 };
